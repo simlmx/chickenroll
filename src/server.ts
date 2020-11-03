6b13cf3ad5011a5d952b8460952cf11db71ba910
Simon Lemieux <lemieux.simon@gmail.com>
2020-11-02 21:05:31 -0500

    Always redirecti to https

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,15 @@ import { Server } from "boardgame.io/server";
 import path from "path";
 import serve from "koa-static";
 import CantStop from "./Game";
+import sslify, {xForwardedProtoResolver} from 'koa-sslify';
 
 const server = Server({
   games: [CantStop],
 });
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
+
+server.app.use(sslify({ resolver: xForwardedProtoResolver }));
 
 // Build path relative to the server.js file
 const frontEndAppBuildPath = path.resolve(__dirname, "../build");
