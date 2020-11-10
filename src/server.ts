1a4e0a2cb0fe5a12cd8be5c8af819e02dae3b80e
Simon Lemieux <lemieux.simon@gmail.com>
2020-11-10 12:15:10 -0500

    Force redirect to $CANTSTOP_HOST
    This way people using the old cantstop.herokuapp.com will be redirected
    to the correct address. We use an environment variable so that it works
    with both staging and prod.

--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,24 @@ const server = Server(serverOptions);
 
 const PORT = env.PORT ? parseInt(env.PORT) : 8000;
 
-server.app.use(sslify({ resolver: xForwardedProtoResolver }));
+// Redirect to CANTSTOP_HOST if the environment variable is defined.
+// In prod we set this to 'CANTSTOP_HOST=canstop.fun' so that people with
+// the old herokuapp address get redirected there.
+const CANTSTOP_HOST = env.CANTSTOP_HOST || undefined;
+
+server.app.use(
+  sslify({ resolver: xForwardedProtoResolver, hostname: CANTSTOP_HOST })
+);
+
+server.app.use(async (ctx, next) => {
+  // If the hostname doesn't match the environment variable, we redirect there.
+  if (CANTSTOP_HOST && ctx.request.header.host !== CANTSTOP_HOST) {
+    ctx.status = 301;
+    ctx.redirect("https://" + CANTSTOP_HOST + ctx.request.url);
+    return;
+  }
+  await next();
+});
 
 // Build path relative to the server.js file
 const frontEndAppBuildPath = path.resolve(__dirname, "../build");
