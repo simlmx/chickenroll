a165438282a33e4fd3cd91affa04295472509857
Simon Lemieux <lemieux.simon@gmail.com>
2020-12-30 17:19:45 -0500

    Simplify the math page

--- src/components/Math.tsx
+++ src/components/Math.tsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { OddsCalculator } from "../math/probs";
-import { Die } from "./Die";
-import { rollDice, pickColor, diceValues2sums } from "../math/probs";
 
 const DEFAULT_SELECTED = [
   false,
@@ -21,20 +19,15 @@ interface NumberProps {
   value: number;
   onMouseDown: () => void;
   selected: boolean;
-  // false = blocked mode
-  isAllowedMode: boolean;
 }
 
 const Number = (props: NumberProps) => {
-  const { value, selected, isAllowedMode, onMouseDown } = props;
+  const { value, selected, onMouseDown } = props;
 
   let className =
     "colFgBottom colFgNotBlocked mathNumber user-select-none pointer";
 
-  if (selected) {
-    // className += " mathNumberSelected";
-    className += isAllowedMode ? " mathNumberAllowed" : " mathNumberForbidden";
-  }
+  className += selected ? " mathNumberAllowed" : " mathNumberForbidden";
 
   if (value > 9) {
     className += " mathNumber2Digits";
@@ -55,23 +48,43 @@ const getIndices = (selected: boolean[], expectedValue?: boolean): number[] => {
   }, []);
 };
 
-const MathDie = (props: { value: number; color: number }) => {
-  const { value, color } = props;
+const probAsEl = (prob: number): JSX.Element => {
+  return (
+    <b>
+      <span title={prob.toString()}>{(100 * prob)?.toFixed(1)}%</span>
+    </b>
+  );
+};
 
-  return <Die {...{ value, color }} />;
+const probLine = (
+  text: JSX.Element,
+  values: number[],
+  colorClassName: string,
+  emoji: JSX.Element,
+  prob: number
+) => {
+  return (
+    <p>
+      {text} <b>{"{"}</b>
+      {values.length === 0
+        ? []
+        : values
+            .map<React.ReactNode>((v, i) => (
+              <span className={colorClassName} key={i}>
+                <b>{v}</b>
+              </span>
+            ))
+            .reduce((prev, curr) => [prev, ", ", curr])}
+      <b>{"}"}</b> = {emoji} = {probAsEl(prob)}
+    </p>
+  );
 };
 
 const Math = () => {
-  const [isAllowedMode, setIsAllowedMode] = useState(true);
-  // const [selected, setSelected] = useState(Array(11).fill(false));
   const [selected, setSelected] = useState<boolean[]>(DEFAULT_SELECTED);
   const [oddsCalculator, setOddsCalculator] = useState<
     OddsCalculator | undefined
   >(undefined);
-
-  const [diceColor, setDiceColor] = useState(pickColor());
-  const [diceValues, setDiceValues] = useState(rollDice(4));
-  const [diceSums, setDiceSums] = useState<number[] | undefined>(undefined); //diceValues2sums(diceValues))
 
   const [prob, setProb] = useState(0);
 
@@ -86,37 +99,15 @@ const Math = () => {
       return;
     }
 
-    if (isAllowedMode) {
-      const allowed = selected.reduce((acc: number[], value, index) => {
-        if (value) {
-          acc.push(index + 2);
-        }
-        return acc;
-      }, []);
-      const prob = oddsCalculator.oddsBust(allowed);
-      setProb(1 - prob);
-    } else {
-      const allowed = selected.reduce((acc: number[], value, index) => {
-        if (!value) {
-          acc.push(index + 2);
-        }
-        return acc;
-      }, []);
-
-      const prob = oddsCalculator.oddsBust(allowed);
-      setProb(prob);
-    }
-  }, [isAllowedMode, oddsCalculator, selected]);
-
-  useEffect(() => {
-    setDiceSums(Array.from(diceValues2sums(diceValues)).sort((a, b) => a - b));
-  }, [diceValues]);
-
-  const changeMode = () => {
-    setIsAllowedMode(!isAllowedMode);
-    // If nothing is selected we keep it that way:
-    // setSelected(selected.map((x) => !x));
-  };
+    const allowed = selected.reduce((acc: number[], value, index) => {
+      if (value) {
+        acc.push(index + 2);
+      }
+      return acc;
+    }, []);
+    const prob = oddsCalculator.oddsBust(allowed);
+    setProb(1 - prob);
+  }, [oddsCalculator, selected]);
 
   const toggleN = (n: number): void => {
     const x = selected.slice();
@@ -131,7 +122,6 @@ const Math = () => {
         onMouseDown={() => toggleN(value - 2)}
         key={value}
         selected={selected[value - 2]}
-        isAllowedMode={isAllowedMode}
       />
     );
   };
@@ -148,127 +138,54 @@ const Math = () => {
 
   const clearButton = (
     <button
-      className="btn btn-dark"
+      className="btn btn-danger btn-sm"
       onClick={() => setSelected(Array(11).fill(false))}
     >
-      Clear
+      Block All
     </button>
   );
 
   const allButton = (
     <button
-      className="btn btn-dark"
+      className="btn btn-success btn-sm"
       onClick={() => setSelected(Array(11).fill(true))}
     >
       Select All
     </button>
   );
 
-  const modeButton = (
-    <button
-      className={
-        isAllowedMode ? "btn btn-sm btn-success" : "btn btn-sm btn-danger"
-      }
-      onClick={() => changeMode()}
-    >
-      {isAllowedMode ? "Climbing Columns" : "Blocked Columns"}
-    </button>
+  const bustEmoji = (
+    <span role="img" aria-label="bust" title="Probability of busting">
+      💥
+    </span>
   );
 
-  const probAsEl = (prob: number): JSX.Element => {
-    return (
-      <b>
-        <span title={prob.toString()}>{(100 * prob)?.toFixed(1)}%</span>
-      </b>
-    );
-  };
-
-  let probEl: JSX.Element;
-
-  const probBustEl = (bust: boolean) => {
-    return (
-      <>
-        {bust ? "" : "not "}
-        <span role="img" aria-label="bust" title="Probability of busting">
-          💥
-        </span>
-      </>
-    );
-  };
+  const okEmoji = (
+    <span role="img" aria-label="bust" title="Probability of busting">
+      👍
+    </span>
+  );
 
   const selectedIndices = getIndices(selected);
+  const unselectdIndices = getIndices(selected, false);
 
-  const probEltext = isAllowedMode
-    ? selectedIndices.length === 1
-      ? "Rolling a sum of"
-      : "Rolling at least one sum from"
-    : "Rolling only sums of";
-
-  probEl = (
+  const probLine1 = probLine(
     <>
-      {selectedIndices.length > 0 && (
-        <p>
-          <>
-            {probEltext}{" "}
-            {selectedIndices
-              .map<React.ReactNode>((t, i) => (
-                <span
-                  className={`${
-                    isAllowedMode ? "text-success" : "text-danger"
-                  }`}
-                  key={i}
-                >
-                  <b>{t}</b>
-                </span>
-              ))
-              .reduce((prev, curr) => [prev, ", ", curr])}
-          </>
-          {/* In red mode we add the probability to bust inline */}
-          {!isAllowedMode && <> = {probBustEl(true)}</>} = {probAsEl(prob)}
-        </p>
-      )}
-      {/* In green mode we add the probability to bust in a differente line */}
-      {selectedIndices.length > 0 && isAllowedMode && (
-        <p>
-          {probBustEl(true)} = {probAsEl(1 - prob)}
-        </p>
-      )}
-    </>
+      Rolling <b>at least one</b> sum from{" "}
+    </>,
+    selectedIndices,
+    "text-success",
+    okEmoji,
+    prob
   );
-
-  const diceOnClick = () => {
-    setDiceValues(rollDice(4));
-    setDiceColor(pickColor());
-  };
-
-  const modeExplanationEl = isAllowedMode ? (
+  const probLine2 = probLine(
     <>
-      <p>
-        What is the probability of rolling <b>at least one</b> of the{" "}
-        <span className="text-success">
-          <b>selected</b>
-        </span>{" "}
-        sums when rolling 4 dice?
-      </p>
-      <p>
-        This is also the probability of <b>not busting</b> when you can only
-        climb those columns.
-      </p>
-    </>
-  ) : (
-    <>
-      <p>
-        What is the probability of rolling <b>only</b> the{" "}
-        <span className="text-danger">
-          <b>selected</b>
-        </span>{" "}
-        sums when rolling 4 dice?
-      </p>
-      <p>
-        This is also the probability of <b>busting on your first roll</b> when
-        those columns are blocked.
-      </p>
-    </>
+      Rolling <b>only</b> sums from{" "}
+    </>,
+    unselectdIndices,
+    "text-danger",
+    bustEmoji,
+    1 - prob
   );
 
   return (
@@ -277,32 +194,27 @@ const Math = () => {
         The Math Behind <i>Can't Stop</i>
       </h1>
       <br />
-
-      <h2>Probabilities of rolling different sums</h2>
+      <h2>Probability of busting</h2>
+      <p>Here you can interactively answer questions like:</p>
       <p>
-        Here you can interactively find the probabilities of rolling given sums
-        when rolling 4 dice.
+        <i>
+          {" "}
+          &nbsp;&nbsp;What are the odds of busting when climbing certain{" "}
+          <span className="text-success">
+            <b>columns</b>
+          </span>
+          ?
+        </i>
       </p>
-      <div className="diceSums user-select-none">
-        <span className="pointer" onMouseDown={diceOnClick}>
-          {diceValues.map((value, i) => (
-            <MathDie value={value} color={diceColor} key={i} />
-          ))}{" "}
-        </span>
-        &nbsp;→&nbsp;
-        <span className="pointer" onMouseDown={diceOnClick}>
-          {diceSums
-            ?.map<React.ReactNode>((t, i) => <b key={i}>{t}</b>)
-            .reduce((prev, curr) => [prev, ", ", curr])}
-        </span>
-      </div>
       <p>
-        <label>
-          <b>Mode: </b>
-        </label>
-        {modeButton}
+        <i>
+          &nbsp;&nbsp;What are the odds of busting on a first roll when some{" "}
+          <span className="text-danger">
+            <b>columns</b>
+          </span>{" "}
+          are blocked?
+        </i>
       </p>
-      {modeExplanationEl}
       <div className="mathWrapWrap">
         <div className="mathWrap row no-gutters">
           <div className="col-sm" />
@@ -314,7 +226,8 @@ const Math = () => {
           <div className="col-sm" />
         </div>
       </div>
-      {probEl}
+      {probLine1}
+      {probLine2}
     </div>
   );
 };
