0591601963fe5033c4c8847f77653cd29e1a5975
Simon Lemieux <lemieux.simon@gmail.com>
2021-01-07 12:24:31 -0500

    Add pencils in lobby to show what is editable

--- src/components/GameSetup.tsx
+++ src/components/GameSetup.tsx
@@ -12,6 +12,29 @@ import getSoundPlayer from "../audio";
 import localStorage from "../utils/localStorage";
 import { ShowProbsType } from "../Game";
 
+// Inspired from: https://github.com/tailwindlabs/heroicons/blob/master/src/solid/pencil.svg
+const Pencil = (props: { color?: number }) => {
+  const { color } = props;
+  let pathClassName = "";
+  if (color != null) {
+    pathClassName = `dotColor${color}`;
+  }
+  return (
+    <div className="pencil">
+      <svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+          className={pathClassName}
+          d="M13.5858 3.58579C14.3668 2.80474 15.6332 2.80474 16.4142 3.58579C17.1953 4.36683 17.1953 5.63316 16.4142 6.41421L15.6213 7.20711L12.7929 4.37868L13.5858 3.58579Z"
+        />
+        <path
+          d="M11.3787 5.79289L3 14.1716V17H5.82842L14.2071 8.62132L11.3787 5.79289Z"
+          className={pathClassName}
+        />
+      </svg>
+    </div>
+  );
+};
+
 // We need this to close the popup when we click outside.
 // https://stackoverflow.com/a/42234988
 function useOutsideAlerter(ref, onClickOutside: () => void) {
@@ -78,7 +101,7 @@ const ColorPicker = (props: ColorPickerProps) => {
           onClick={() => setVisible(true)}
           disabled={disabled}
         >
-          <div>◢</div>
+          {!disabled && <Pencil color={color} />}
         </button>
       </div>
       {visible && (
@@ -218,6 +241,7 @@ export const Player = (props: PlayerProps): JSX.Element => {
         onClick={(e) => !currentReady && setEditingName(true)}
       >
         {name || "..."}
+        {!currentReady && itsMe && <Pencil />}
       </div>
     );
   }
